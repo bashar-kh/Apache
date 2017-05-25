8fbf8ec052fc01b57aa6967e906303c495331c83
vagrant <bashar.alkhateeb@gmail.com>
2017-05-25 16:39:11 +0000

    change httpd to Apache2

--- recipes/server.rb
+++ recipes/server.rb
@@ -3,15 +3,16 @@
 # Recipe:: server
 #
 # Copyright (c) 2017 The Authors, All Rights Reserved.
-package 'httpd' do 
-	Action :install
+
+package 'apache2' do 
+	action :install
 end
 
 file '/var/www/html/index.html' do
 	content '<h1>Hello ,World!</h1>'
 end
 
-service 'httpd' do
-	action [:enable , :start]
+service 'apache2' do
+	action [ :enable, :start ]
 end
 
