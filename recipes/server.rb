59bdb3ad457a2083a8bff9402343e831ca714e80
vagrant <bashar.alkhateeb@gmail.com>
2017-05-26 21:44:57 +0000

    adding Ohai parameters to HTML

--- recipes/server.rb
+++ recipes/server.rb
@@ -9,10 +9,14 @@ package 'apache2' do
 end
 
 file '/var/www/html/index.html' do
-	content '<h1>Hello ,World!</h1>'
+	content "<h1>Hello ,World!</h1>
+		<br>HOSTNAME : #{node['hostname']}
+		<br>CPU : #{node['cpu']['0']['mhz']}
+		<br>MEMORY: #{node['memory']['total']}
+		<br>IPADDRESS: #{node['ipaddress']}
+"
 end
 
 service 'apache2' do
 	action [ :enable, :start ]
 end
-
